9316b23a3453658617808033466cbaadabe0ce79
wangxiaochi <wangxiaochi@baidu.com>
2015-01-28 21:24:27 +0800

    complete

--- Server.php
+++ Server.php
@@ -8,6 +8,7 @@ class Server
     public $address = '0.0.0.0';
     public $port = 8089;
 
+    private $pool = [];
     public function start()
     {
 
@@ -32,20 +33,20 @@ class Server
             return false;
         }
         pcntl_signal(SIGINT, function ($signo) use($sock) {
-            echo "u ctrl+c";
+            echo "u ctrl+c\n";
             \socket_close($sock);
             exit();
         });
         $null = null;
-        $pool = [];
+        $this->pool[] = $sock;
         do {
             pcntl_signal_dispatch();
-            $arr = [$sock];
-            $select = \socket_select($arr, $null, $null, 0);
+            $left = $this->pool;
+            $select = \socket_select($left, $null, $null, 0);
             if ($select === false) {
                 echo "\socket_select() failed: reason: " . \socket_strerror(\socket_last_error($sock)) . "\n";
                 $this->error = \socket_last_error();
-                \socket_close($sock);
+                $this->closePool();
                 return false;
             }
             if ($select === 0) {
@@ -53,59 +54,52 @@ class Server
                 usleep(100);
                 continue;
             }
-            if (($msgsock = \socket_accept($sock)) === false) {
-                echo "\socket_accept() failed: reason: " . \socket_strerror(\socket_last_error($sock)) . "\n";
-                break;
-            }
-            if (\socket_set_nonblock($msgsock) === false) {
-                $this->error = \socket_last_error();
-                \socket_close($msgsock);
-                \socket_close($sock);
-                return false;
-            }
-            echo "a client connect\n";
-            $this->raise('connect', $msgsock);
-            $pool[] = $msgsock;
-
-            do {
-                pcntl_signal_dispatch();
-                $left = $pool;
-                echo "before select "; var_dump($left);
-                $select = \socket_select($left, $null, $null, 0);
-                echo " after select "; var_dump($left);
-                if ($select === false) {
-                    echo "\socket_select() failed: reason: " . \socket_strerror(\socket_last_error($msgsock)) . "\n";
-                    $this->error = \socket_last_error();
-                    $this->closePool($pool);
-                    \socket_close($sock);
-                    return false;
-                }
-                if ($select === 0) {
-                    echo "\r inner run ".time();
-                    usleep(100);
-                    continue;
-                }
-                foreach ($left as $msgsock) {
-                    echo "$select selected\n";
+            foreach ($left as $msgsock) {
+                if ($msgsock === $sock) {
+                    echo "main sock\n";
+                    if (($msgsock = \socket_accept($sock)) === false) {
+                        echo "\socket_accept() failed: reason: " . \socket_strerror(\socket_last_error($sock)) . "\n";
+                        break;
+                    }
+                    if (\socket_set_nonblock($msgsock) === false) {
+                        $this->error = \socket_last_error();
+                        $this->closePool();
+                        return false;
+                    }
+                    echo "a client connect\n";
+                    $this->raise('connect', $msgsock);
+                    $this->pool[] = $msgsock;
+                } else {
+                    echo "pool sock\n";
                     if (false === (\socket_recv($msgsock, $buf, 2048, MSG_DONTWAIT))) {
                         echo "\socket_recv() failed: reason: " . \socket_strerror(\socket_last_error($msgsock)) . "\n";
                         break 2;
                     }
                     $action = $this->raise('receive', $msgsock, $buf);
                     if ($action === false) {
-                        break;
-                    } elseif ($action === true) {
-                        continue;
+                        $this->removeChild($msgsock);
                     }
                 }
-            } while (true);
-            \socket_close($msgsock);
+            }
         } while (true);
         $this->closePool($pool);
     }
-    private function closePool($pool)
+    private function removeChild($socket)
     {
-        foreach ($pool as $s) {
+        reset($this->pool);
+        while (($key = key($this->pool)) !== null) {
+            if (current($this->pool) === $socket) {
+                echo "delete\n";
+                socket_close($socket);
+                unset($this->pool[$key]);
+                break;
+            }
+            next($this->pool);
+        }
+    }
+    private function closePool()
+    {
+        foreach ($this->pool as $s) {
             \socket_close($s);
         }
     }
