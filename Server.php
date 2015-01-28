0cc71d5924cf631481bb0af31ebc82481b2a889b
wangxiaochi <wangxiaochi@baidu.com>
2015-01-28 19:55:28 +0800

    connect

--- Server.php
+++ Server.php
@@ -9,6 +9,7 @@ class Server
     public $port = 8089;
     public function start()
     {
+
         /* Turn on implicit output flushing so we see what we're getting
          * as it comes in. */
         ob_implicit_flush();
@@ -17,26 +18,35 @@ class Server
             $this->error = \socket_last_error();
             return false;
         }
-
+        if (\socket_set_nonblock($sock) === false) {
+            $this->error = \socket_last_error();
+            return false;
+        }
         if (\socket_bind($sock, $this->address, $this->port) === false) {
             $this->error = \socket_last_error();
             return false;
         }
-
         if (\socket_listen($sock, 5) === false) {
             $this->error = \socket_last_error();
             return false;
         }
-
+        pcntl_signal(SIGINT, function ($signo) use($sock) {
+            echo "u ctrl+c";
+            \socket_close($sock);
+            exit();
+        });
         do {
+            pcntl_signal_dispatch();
+            $arr = [$sock];
+            if (!\socket_select($arr, $arr, $arr, 0)) {
+                continue;
+            }
             if (($msgsock = \socket_accept($sock)) === false) {
                 echo "\socket_accept() failed: reason: " . \socket_strerror(\socket_last_error($sock)) . "\n";
                 break;
             }
-            /* Send instructions. */
-            $msg = "\nWelcome to the PHP Test Server. \n" .
-                "To quit, type 'quit'. To shut down the server type 'shutdown'.\n";
-            \socket_write($msgsock, $msg, strlen($msg));
+            
+            $this->raise('connect', $msgsock);
 
             do {
                 if (false === ($buf = \socket_read($msgsock, 2048, PHP_NORMAL_READ))) {
@@ -62,8 +72,14 @@ class Server
 
         \socket_close($sock);
     }
+    public function raise($event, $socket)
+    {
+        if (isset($this->events[$event])) {
+            $this->events[$event]($socket);
+        }
+    }
     public function on($event, $callback)
     {
-
+        $this->events[$event] = $callback;
     }
 }
--- test.php
+++ test.php
@@ -5,4 +5,13 @@ require 'Server.php';
 use Tangle\Server;
 
 $s = new Server();
-$s->start();
+$s->on('connect', function ($msgsock) {
+	/* Send instructions. */
+    $msg = "\nWelcome to the PHP Test Server. \n" .
+        "To quit, type 'quit'. To shut down the server type 'shutdown'.\n";
+    \socket_write($msgsock, $msg, strlen($msg));
+});
+if ($s->start() === false) {
+	echo "failed: reason: " . \socket_strerror(\socket_last_error($sock)) . "\n";
+	exit(1);
+}
